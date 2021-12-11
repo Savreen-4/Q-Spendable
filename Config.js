54004de53644812942ebc5af4f2f121dfec906bf
Savreen-4 <79464605+Savreen-4@users.noreply.github.com>
2021-12-11 20:16:43 +0530

    Add files via upload
    qss techno spendable

---
+++ Config.js
@@ -0,0 +1,11 @@
+// export const API_URL = "http://3.18.90.217:8443";
+
+export const API_URL = " https://2979f2c99add.ngrok.io";
+// export const API_URL = "https://gimmeit.net.au:8443";
+// export const API_URL = "http://localhost:8443";
+export const URLDATA = {
+  url: API_URL + "/graphql",
+  queryFile: ""
+};
+
+export const Image_GETURL = API_URL + "/api/containers/images/download/";
